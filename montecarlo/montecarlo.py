57fc1e7a54e2a57d7cca6176c9e6660be7031631
Lucy-Shichman <lgs4zj@virginia.edu>
2024-07-05 10:38:48 -0400

    FP3 participation

--- montecarlo/montecarlo.py
+++ montecarlo/montecarlo.py
@@ -1,6 +1,56 @@
+import numpy as np
+import pandas as pd
+
 class Die():
-    def __init__():
-        pass
+     """
+     This class creates a Die object, changes it's weight, 
+     rolls it, and returns it's current state.
+     """
+    def __init__(self, faces):
+        """
+        initializes faces, weights, and die attributes
+        """  
+        if type(faces) != np.ndarray:
+            raise TypeError("Faces input must be a NumPy array")
+        
+        if len(faces) != len(np.unique(faces)):
+            raise ValueError("Array values must be distinct")
+        
+        self.faces = faces
+        self.weights = [1 for i in faces]
+        self._die = pd.DataFrame({
+            'weights' : self.weights
+        }).set_index(self.faces)
+    
+    def change_weight(self, face_value, new_weight):
+        """
+        method to change the weight of a single side
+        """
+        if face_value not in self.faces:
+            raise IndexError("face_value input is not a valid value")
+        
+        try:
+            float(new_weight)
+        except:
+            raise TypeError("new_weight input is not a valid type")
+        
+        self._die.loc[face_value] = new_weight
+    
+    def roll_die(self, num_rolls=1):
+        """
+        method to roll the die one or more times
+        """
+        outcomes = []
+        for i in range(num_rolls):
+            outcome = self._die.sample(weights=self._die.weights).index[0]
+            outcomes.append(outcome)
+        return outcomes
+    
+    def current_state(self):
+        """
+        method to show the die's current state
+        """
+        return self._die
 
 class Game():
     def __init__():
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='montecarlo package',
+    version='1.0.0',
+    url='https://github.com/Lucy-Shichman/DS5100-finalproject-lgs4zj',
+    author='Lucy Shichman',
+    author_email='lgs4zj@virginia.edu',
+    description='This package contains the montecarlo classes',
+    packages=['montecarlo']
+)
